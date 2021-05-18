b3dde34f0c437946381917d02d541f61e35801d7
akash elhance <akashelhance@gmail.com>
2021-05-18 17:08:00 +0530

    CRUD

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 const models = require('../models');
 
+//C
 const createDish = async (req, res) => {
   try {
     const dish = await models.Dish.create(req.body);
@@ -11,7 +12,7 @@ const createDish = async (req, res) => {
   }
 }
 
-
+//R
 const getAllDishes = async (req, res) => {
     try {
       const dishes = await models.Dish.findAll({});
@@ -20,9 +21,65 @@ const getAllDishes = async (req, res) => {
       return res.status(500).send(error.message);
     }
   }
+//R -> Single Dish
+const getDishById = async (req, res) => {
+    try {
+      const { DishId } = req.params;
+      const dish = await models.Dish.findOne({
+        where: { id: DishId },
+      });
+      if (dish) {
+        return res.status(200).json({ dish });
+      }
+      return res.status(404).json({"Message": 'Dish with the specified ID does not exists'})
+    } catch (error) {
+        return res.status(500).json({"Message":error.message});
+    }
+  }
+
+
+//U
+
+const updateDish = async (req, res) => {
+    try {
+      const { DishId } = req.params;
+      const [ updated ] = await models.Dish.update(req.body, {
+        where: { id: DishId }
+      });
+      if (updated) {
+        const updatedDish = await models.Dish.findOne({ where: { id: DishId } });
+        return res.status(200).json({ dish: updatedDish });
+      }
+      throw new Error('Dish not found');
+    } catch (error) {
+        return res.status(500).json({"Message":error.message});
+    }
+  };
+
+//D:
+
+const deleteDish = async (req, res) => {
+    try {
+      const { DishId } = req.params;
+      const deleted = await models.Dish.destroy({
+        where: { id: DishId }
+      });
+      if (deleted) {
+        return res.status(204).json({"Message": "Dish deleted"});
+      }
+      throw new Error("Dish not found");
+    } catch (error) {
+      return res.status(500).json({"Message":error.message});
+    }
+  };
+
 
 module.exports = {
   createDish,
   getAllDishes,
-  
+  getDishById ,
+  updateDish,
+  deleteDish,
+
+
 }
--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,10 @@ router.get('/', (req, res) => res.json({"message": "Api Route"}))
 
 router.post('/dish', controllers.createDish);
 router.get('/dish', controllers.getAllDishes);
+router.get('/dish/:DishId', controllers.getDishById);
+router.put('/dish/:DishId', controllers.updateDish);
+router.delete('/dish/:DishId', controllers.deleteDish);
+
+
 
 module.exports = router;
